818190afa45abe3d9999e4084c6e3b68770a88ba
nani-dev <jnkocjan@yahoo.com>
2024-07-10 20:35:23 +0200

    Entitiy framework for SQL

---
+++ Data/DataContextEF.cs
@@ -0,0 +1,29 @@
+﻿using Learning_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecondCode.Data
+{
+    public class DataContextEF : DbContext
+    {
+        public DbSet<Exercise>? Exercise { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                // Pass the connection, in case of failure, retry
+                options.UseSqlServer(
+                    "Server=localhost;Database=ExerciseDatabase;" +
+                    "TrustServerCertificate=true;Trusted_Connection=true;",
+                    options => options.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("TutorialAppSchema");
+            modelBuilder.Entity<Exercise>();
+        }
+        
+    }
+}
